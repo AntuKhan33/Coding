beef9813c80cea12d782bf96825f6791112075e6
AntuKhan33 <81099161+AntuKhan33@users.noreply.github.com>
2022-04-24 03:11:16 +0600

    Add files via upload

---
+++ C# Project/Complete Pharmacy Student Management System/Pharmacy Management System/Pharmacy Management System/AdministratorUC/UC_Dashboard.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Pharmacy_Management_System.AdministratorUC
+{
+    partial class UC_Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_Dashboard));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Adminlabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Pharlabel = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSync = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(446, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dashboard";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Administrator";
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.Adminlabel);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(136, 157);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 611);
+            this.panel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(2, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(322, 300);
+            this.label6.TabIndex = 7;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(246, 104);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Task Perfromed\r\n     *Add User\r\n     *View User\r\n     *Profile Management";
+            // 
+            // Adminlabel
+            // 
+            this.Adminlabel.AutoSize = true;
+            this.Adminlabel.BackColor = System.Drawing.Color.White;
+            this.Adminlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Adminlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adminlabel.Location = new System.Drawing.Point(116, 539);
+            this.Adminlabel.Name = "Adminlabel";
+            this.Adminlabel.Size = new System.Drawing.Size(45, 49);
+            this.Adminlabel.TabIndex = 5;
+            this.Adminlabel.Text = "0";
+            this.Adminlabel.Click += new System.EventHandler(this.Adminlabel_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(60, 513);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(186, 26);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Total No of Admin";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Controls.Add(this.Pharlabel);
+            this.panel3.Controls.Add(this.label14);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(642, 154);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(328, 611);
+            this.panel3.TabIndex = 4;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(3, 55);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(288, 104);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Task Perfromed\r\n     *Add Medicines\r\n     *View and Sell Medicines\r\n     *Medicin" +
+    "e Management ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(3, 173);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(310, 325);
+            this.label10.TabIndex = 7;
+            this.label10.Text = resources.GetString("label10.Text");
+            // 
+            // Pharlabel
+            // 
+            this.Pharlabel.AutoSize = true;
+            this.Pharlabel.BackColor = System.Drawing.Color.White;
+            this.Pharlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Pharlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pharlabel.Location = new System.Drawing.Point(125, 539);
+            this.Pharlabel.Name = "Pharlabel";
+            this.Pharlabel.Size = new System.Drawing.Size(45, 49);
+            this.Pharlabel.TabIndex = 6;
+            this.Pharlabel.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(47, 513);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(233, 26);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "Total No of Pharmacist";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Pharmacist";
+            // 
+            // btnSync
+            // 
+            this.btnSync.AccessibleName = "btnSync";
+            this.btnSync.CheckedState.Parent = this.btnSync;
+            this.btnSync.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSync.CustomImages.Parent = this.btnSync;
+            this.btnSync.FillColor = System.Drawing.Color.White;
+            this.btnSync.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSync.ForeColor = System.Drawing.Color.White;
+            this.btnSync.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnSync.HoverState.Parent = this.btnSync;
+            this.btnSync.Image = global::Pharmacy_Management_System.Properties.Resources.sync_64px;
+            this.btnSync.ImageSize = new System.Drawing.Size(35, 35);
+            this.btnSync.Location = new System.Drawing.Point(534, 67);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.PressedColor = System.Drawing.Color.White;
+            this.btnSync.ShadowDecoration.Parent = this.btnSync;
+            this.btnSync.Size = new System.Drawing.Size(45, 53);
+            this.btnSync.TabIndex = 7;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Pharmacy_Management_System.Properties.Resources.sync;
+            this.pictureBox1.Location = new System.Drawing.Point(506, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UC_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "UC_Dashboard";
+            this.Size = new System.Drawing.Size(1089, 768);
+            this.Load += new System.EventHandler(this.UC_Dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Pharlabel;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label Adminlabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2Button btnSync;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
